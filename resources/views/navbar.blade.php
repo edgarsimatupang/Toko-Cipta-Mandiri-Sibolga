2a3a99cdf04e08d2b5a124ea2cba9629d6cba0b4
tian0900 <82864213+tian0900@users.noreply.github.com>
2022-07-30 11:04:29 +0700

    first commit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -22,7 +22,7 @@
         <header>
             <h1 class="site-heading text-center text-faded d-none d-lg-block">
                 <span class="site-heading-upper text-primary mb-3">Toko Cipta Mandiri Sibolga</span>
-                <span class="site-heading-lower">Independent Creation Shop</span>
+                <span class="site-heading-lower">TOSERBA</span>
             </h1>
         </header>
         <!-- Navigation-->
