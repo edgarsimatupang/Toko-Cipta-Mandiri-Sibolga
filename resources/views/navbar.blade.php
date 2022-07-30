213e8627ae57d4d41c4aee052e2f429a10af7d02
Edgar_11 <edgarvictoria2020@gmail.com>
2022-07-30 11:43:47 +0700

    first commit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -22,7 +22,7 @@
         <header>
             <h1 class="site-heading text-center text-faded d-none d-lg-block">
                 <span class="site-heading-upper text-primary mb-3">Toko Cipta Mandiri Sibolga</span>
-                <span class="site-heading-lower">TOKO CIPTA MANDIRI SIBOLGA</span>
+                <span class="site-heading-lower">Sibolga Independent Creation Shop</span>
             </h1>
         </header>
         <!-- Navigation-->
